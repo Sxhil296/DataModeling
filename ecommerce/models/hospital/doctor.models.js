f2bd8ca56d6ba6a7ad6dee8ec24d3801be099a4e
Sahil <leviack629@gmail.com>
2023-11-14 17:12:06 +0530

    hospital management  data modeling

--- ecommerce/models/hospital/doctor.models.js
+++ ecommerce/models/hospital/doctor.models.js
@@ -1,5 +1,48 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalHoursSchema = new mongoose.Schema({
+    hospitalName:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hospital",
+        required:true
+    },
+    workingHours: {
+        type:Number,
+        required:true
+    }
+})
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experienceInYears: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    worksInHospitals: [
+        {
+            hospital: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Hospital",
+                required: true
+            },
+            hours: [hospitalHoursSchema]
+        }
+    ]
+  },
+  { timestamps: true }
+);
 
 export const Doctor = mongoose.model("Doctor", doctorSchema);
